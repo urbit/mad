f89c74f274e26025e3a540b07d2970cbb8105cfc
Josh Lehman <josh@urbit.org>
2024-04-11 06:24:05 -0700

    wip

--- src/data.clj
+++ src/data.clj
@@ -6,34 +6,186 @@
 (def ec-urbit
   "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/u/urbit.toml")
 
+(def ec-filecoin
+  "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/f/filecoin.toml")
+
+(def ec-near
+  "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/n/near.toml")
+
+;; top 20
+(def ec-sui
+  "")
+
+(def ec-gnosis
+  "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/g/gnosis.toml")
+
+(def ec-chainlink
+  "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/c/chainlink.toml")
+
+;; top 40
+(def ec-stellar
+  "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/s/stellar.toml")
+
+;; top 50
+(def ec-eos
+  "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/e/eos.toml")
+
+(defn sub-ecosystem->raw-toml
+  [sub]
+  (let [c (clojure.string/lower-case (first sub))]
+    (format "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/%s/%s.toml"
+            c
+            (-> sub
+                clojure.string/lower-case
+                (clojure.string/replace "." "-")
+                (clojure.string/replace " " "-")))))
 
 (defn extract-owner-repo [s]
-  (->> (clojure.string/split s #"/")
-       (take-last 2)))
+  (let [[owner repo]
+        (->> (clojure.string/split s #"/")
+             (take-last 2))]
+    {:owner owner :repo repo}))
 
 
-(defn fetch-ec-urbit-repos
+
+(defn- format-ec-repos
+  [project ecosystem repos]
+  (map (comp #(assoc % :project project :ecosystem ecosystem)
+             extract-owner-repo
+             #(get % "url"))
+       repos))
+
+
+(defn fetch-ec-repos
   "Utility used to bootstrap the set of repositories."
-  []
-  (let [repos (-> (client/get ec-urbit)
-                  :body
-                  toml/read
-                  (get "repo"))]
-    (map (comp extract-owner-repo #(get % "url")) repos)))
+  [root-url project & [ecosystem]]
+  (try
+    (let [res   (-> (client/get root-url)
+                    :body
+                    toml/read)
+          repos (format-ec-repos project
+                                 (or ecosystem project)
+                                 (get res "repo"))]
+      (loop [repos repos
+             sub-ecos (get res "sub_ecosystems")]
+        (if (empty? sub-ecos)
+          repos
+          (recur
+           (let [sub-eco (first sub-ecos)
+                 sub-url (sub-ecosystem->raw-toml sub-eco)]
+             (println
+              (format "Fetching repos for subecosystem %s within project %s at %s:"
+                      sub-eco project sub-url))
+             (concat repos (fetch-ec-repos sub-url project sub-eco)))
+           (rest sub-ecos)))))
+    (catch Exception e
+      (println e)
+      [])))
 
 
 ;; TODO: ensure that electric capital's data is updated per the below, which has
+
+(def urbit-repo-branches
+  {"urwasm" ["lia"]
+   "ares" ["eamsden/codegen" "eamsden/codegen-old" "msl/codegen"]
+   "urbit" ["develop" "lf/neo" "yu/sema" "jb/plot" "yu/sema-mesa"
+            "next/kelvin/410"]
+   "vere" ["develop" "jb/plot" "lf/xmas" "yu/mesa" "next/kelvin/410"]})
+
 ;; deviated
 (def known-repositories
-  [{:owner "ashelkovnykov", :repo "urbit-hosting"}
+  [{:owner "midden-fabler" :repo "phoenix-wip"}
+   {:owner "midden-fabler" :repo "ahoy"}
+   {:owner "midden-fabler" :repo "fleet"}
+   {:owner "tiller-tolbus" :repo "chain"}
+   {:owner "arthyn" :repo "gin-tonic"}
+   {:owner "deathtothecorporation" :repo "make"}
+   {:owner "deathtothecorporation" :repo "mask"}
+   {:owner "darighost" :repo "cen-chan"}
+   {:owner "Anseta" :repo "urbit-tea"}
+   {:owner "archetype-org" :repo "clack"}
+   {:owner "youfoundron" :repo "urbit-academy"}
+   {:owner "sidnym-ladrut" :repo "camp"}
+   {:owner "sidnym-ladrut" :repo "elogin"}
+   {:owner "jamesacklin" :repo "urbit-webhook"}
+   {:owner "paglud-nodsyn" :repo "oracle"}
+   {:owner "bohendo" :repo "bizbaz"}
+   {:owner "Quodss" :repo "wasm-hackathon"}
+   {:owner "Quodss"
+    :repo "urwasm"
+    :branches ["lia"]}
+   {:owner "walter-hoddel" :repo "mail"}
+   {:owner "supercoolyun" :repo "grip"}
+   {:owner "supercoolyun" :repo "grip-sample"}
+   {:owner "namful-mocwen" :repo "yijing"}
+   {:owner "dislux-hapfyl" :repo "h00nxfu"}
+   {:owner "salbaroudi" :repo "Quoridor"}
+   {:owner "urbitme" :repo "urbit-ob-rs"}
+   {:owner "urbitme" :repo "sigil-phonemes"}
+   {:owner "yungcalibri" :repo "pharos"}
+   {:owner "sidnym-ladrut" :repo "urbitswap"}
+   {:owner "urbitswap" :repo "urbitswap"}
+   {:owner "Native-Planet" :repo "onyx"}
+   {:owner "fischsauce" :repo "delta"}
+   {:owner "dmvianna" :repo "delta"}
+   {:owner "neonfuz" :repo "urlingo"}
+   {:owner "ngzax" :repo "urbit-sync"}
+   {:owner "JohnRillos" :repo "wiki"}
+   {:owner "Native-Planet" :repo "penpAI"}
+   {:owner "Native-Planet" :repo "aqmon"}
+   {:owner "patosullivan" :repo "trackur"}
+   {:owner "salbaroudi" :repo "Quoridor"}
+   {:owner "pilwex-migsun" :repo "torb"}
+   {:owner "ryjm" :repo "boat"}
+   {:owner "fischsauce" :repo "app-school"}
+   {:owner "ilyakooo0" :repo "seax"}
+   {:owner "nallux-dozryl" :repo "nock"}
+   {:owner "bohendo" :repo "hsl"}
+   {:owner "chrisalexadams" :repo "collective"}
+   {:owner "LaconicNetwork" :repo "awesome-urbit"}
+   {:owner "structure-group" :repo "albums"}
+   {:owner "gmcz" :repo "cals"}
+   {:owner "holium" :repo "desks"}
+   {:owner "holium" :repo "realm"}
+   {:owner "latter-bolden" :repo "pier-transfer"}
+   {:owner "xywei" :repo "uforth"}
+   {:owner "JohnRillos" :repo "urbit_template"}
+   {:owner "urbit-pilled" :repo "hoon-ts-editors"}
+   {:owner "bonbud-macryg" :repo "rss-sub"}
+   {:owner "mbcladwell" :repo "gurbit"}
+   {:owner "yungcalibri" :repo "prism"}
+   {:owner "altugbakan" :repo "hsl"}
+   {:owner "bzol" :repo "cosmos"}
+   {:owner "bzol" :repo "cosmos"}
+   {:owner "mnrrxyz" :repo "HSL"}
+   {:owner "pasnec-salmyr" :repo "hsl"}
+   {:owner "parkbanks" :repo "hsl"}
+   {:owner "tamlut-modnys" :repo "curriculum"}
+   {:owner "JR-Vickers" :repo "hoon_school_exercises"}
+   {:owner "jpfeiffer16" :repo "hoonschool"}
+   {:owner "mbcladwell" :repo "bookstore"}
+   {:owner "sidnym-ladrut" :repo "desk"}
+   {:owner "bzol" :repo "hitler"}
+   {:owner "salbaroudi" :repo "HoonSchool_06.2023"}
+   {:owner "urbit-pilled" :repo "tree-sitter-hoon"}
+   {:owner "urbit-pilled" :repo "vscode-hoon"}
+   {:owner "toddux-dopnel" :repo "excalidraw-for-urbit"}
+   {:owner "tamlut-modnys" :repo "yijingbot"}
+   {:owner "tamlut-modnys" :repo "userspace-util"}
+   {:owner "ashelkovnykov", :repo "urbit-hosting"}
    {:owner "forchesta", :repo "urbit-dev"}
    {:owner "liam-fitzgerald", :repo "urbit-openai"}
    {:owner "matthew-levan", :repo "tell"}
+   {:owner "matthiasschaub", :repo "tahuti"}
+   {:owner "matthiasschaub", :repo "pilothouse"}
+   {:owner "mrdomino" :repo "urbit-sysops"}
    {:owner "nodreb-borrus", :repo "sigil-render"}
    {:owner "hanryc-tirpur", :repo "trail-mobile"}
    {:owner "hanryc-tirpur", :repo "trail"}
    {:owner "cyclomancer", :repo "bet"}
    {:owner "cyclomancer", :repo "ccrur"}
+   {:owner "cyclomancer", :repo "volt"}
+   {:owner "worpet-bildet", :repo "portal"}
    {:owner "dalten-collective", :repo "agent-skeleton"}
    {:owner "dalten-collective", :repo "books"}
    {:owner "dalten-collective", :repo "cgol"}
@@ -65,6 +217,7 @@
    {:owner "Fang-", :repo "suite"}
    {:owner "gusmacaulay", :repo "atlas"}
    {:owner "gusmacaulay", :repo "dukebox-glob"}
+   {:owner "gusmacaulay", :repo "mentat"}
    {:owner "h5gq3", :repo "graph-query"}
    {:owner "hanfel-dovned", :repo "Bless"}
    {:owner "hanfel-dovned", :repo "Click"}
@@ -89,10 +242,13 @@
    {:owner "hosted-fornet", :repo "whitelist"}
    {:owner "jackfoxy", :repo "urQL"}
    {:owner "johnhyde", :repo "astrolabe"}
+   {:owner "johnhyde", :repo "turf"}
+   {:owner "johnhyde", :repo "monkey"}
    {:owner "midlyx-hatrys", :repo "urbit-cob"}
    {:owner "midsum-salrux", :repo "faux"}
    {:owner "midsum-salrux", :repo "gato"}
    {:owner "midsum-salrux", :repo "tendiebot"}
+   {:owner "mopfel-winrux", :repo "sand"}
    {:owner "mopfel-winrux", :repo "NockPU"}
    {:owner "mopfel-winrux", :repo "urbit-umbrel"}
    {:owner "Native-Planet", :repo "anchor"}
@@ -100,6 +256,7 @@
    {:owner "Native-Planet", :repo "GroundSeg"}
    {:owner "Native-Planet", :repo "urbit-casaos"}
    {:owner "Native-Planet", :repo "uart-agent"}
+   {:owner "Native-Planet", :repo "urbit-docker"}
    {:owner "niblyx-malnus", :repo "nested-goals"}
    {:owner "niblyx-malnus", :repo "surf"}
    {:owner "niblyx-malnus", :repo "goals"}
@@ -112,6 +269,7 @@
    {:owner "orlin" :repo "urbit-desk-thing"}
    {:owner "j3-productions", :repo "quorum"}
    {:owner "sidnym-ladrut", :repo "pantheon"}
+   {:owner "sigilante", :repo "atalante"}
    {:owner "sigilante", :repo "l10n"}
    {:owner "sigilante", :repo "lazytrig"}
    {:owner "sigilante", :repo "mush"}
@@ -125,6 +283,7 @@
    {:owner "thecommons-urbit", :repo "chess"}
    {:owner "litlep-nibbyt", :repo "forums"}
    {:owner "tinnus-napbus", :repo "base16-hoon"}
+   {:owner "tinnus-napbus", :repo "files"}
    {:owner "tinnus-napbus", :repo "beacon"}
    {:owner "tinnus-napbus", :repo "docs-app"}
    {:owner "tinnus-napbus", :repo "sentinel"}
@@ -135,31 +294,20 @@
    {:owner "tloncorp", :repo "bouncer"}
    {:owner "tloncorp", :repo "hoon-vscode"}
    {:owner "tloncorp", :repo "landscape-apps"}
+   {:owner "tloncorp", :repo "tlon-apps"}
    {:owner "tloncorp", :repo "landscape"}
    {:owner "tloncorp", :repo "mock-http-api"}
    {:owner "tloncorp", :repo "eyrie"}
-   {:owner "uqbar-dao", :repo "dao-contract"}
-   {:owner "uqbar-dao", :repo "escape-app"}
-   {:owner "uqbar-dao", :repo "galaxy-girls"}
-   {:owner "uqbar-dao", :repo "indexer-ui"}
-   {:owner "uqbar-dao", :repo "network-age"}
-   {:owner "uqbar-dao", :repo "phonebook"}
-   {:owner "uqbar-dao", :repo "rdbms"}
-   {:owner "uqbar-dao", :repo "nectar"}
-   {:owner "uqbar-dao", :repo "sirens-of-uqbar"}
-   {:owner "uqbar-dao", :repo "react-native-api"}
-   {:owner "uqbar-dao", :repo "ui-components"}
-   {:owner "uqbar-dao", :repo "uqbar-core"}
-   {:owner "uqbar-dao", :repo "urbit-mobile-app-template"}
-   {:owner "uqbar-dao", :repo "urbit-ui-template"}
-   {:owner "uqbar-dao", :repo "wallet-ui"}
-   {:owner "uqbar-dao", :repo "ziggurat"}
-   {:owner "uqbar-dao", :repo "ziggurat-ui"}
-   {:owner "uqbar-dao", :repo "pongo"}
-   {:owner "uqbar-dao", :repo "pongo-app"}
-   {:owner "uqbar-dao", :repo "fire-hose"}
-   {:owner "uqbar-dao", :repo "dev-suite"}
-   {:owner "uqbar-dao", :repo "screed"}
+   {:owner "tloncorp", :repo "hoon-format"}
+   {:owner "urbit" :repo "urcrypt-sys"}
+   {:owner "urbit" :repo "docs.urbit.org"}
+   {:owner "urbit" :repo "hits"}
+   {:owner "urbit" :repo "urbit.foundation"}
+   {:owner "urbit" :repo "UIPs"}
+   {:owner "urbit" :repo "NEAR"}
+   {:owner "urbit" :repo "create-near-app"}
+   {:owner "urbit" :repo "NearSocialVM"}
+   {:owner "urbit" :repo "Aegean"}
    {:owner "urbit", :repo "antechamber"}
    {:owner "urbit", :repo "archaeology"}
    {:owner "urbit", :repo "archaeology-factor"}
@@ -216,7 +364,9 @@
    {:owner "urbit", :repo "network-api"}
    {:owner "urbit", :repo "network-api-deprecated"}
    {:owner "urbit", :repo "network-explorer"}
-   {:owner "urbit", :repo "ares"}
+   {:owner "urbit"
+    :repo "ares"
+    :branches ["eamsden/codegen" "eamsden/codegen-old" "msl/codegen"]}
    {:owner "urbit", :repo "nock-js"}
    {:owner "urbit", :repo "nockjs"}
    {:owner "urbit", :repo "noun"}
@@ -227,7 +377,7 @@
    {:owner "urbit", :repo "phonemes-js"}
    {:owner "urbit", :repo "plan"}
    {:owner "urbit", :repo "pleac-hoon"}
-   {:owner "urbit", :repo "port"}
+   {:owner "latter-bolden", :repo "port"}
    {:owner "urbit", :repo "pottery"}
    {:owner "urbit", :repo "preview"}
    {:owner "urbit", :repo "profile"}
@@ -250,7 +400,13 @@
    {:owner "urbit", :repo "tree"}
    {:owner "urbit", :repo "up8-ticket"}
    {:owner "urbit", :repo "urb"}
-   {:owner "urbit", :repo "urbit"}
+   {:owner "urbit",
+    :repo "urbit"
+    :branches ["develop" "lf/neo" "yu/sema" "jb/plot" "yu/sema-mesa"
+               "next/kelvin/410"]}
+   {:owner "urbit"
+    :repo "vere"
+    :branches ["develop" "jb/plot" "lf/xmas" "yu/mesa" "next/kelvin/410"]}
    {:owner "urbit", :repo "urbit-bitcoin-rpc"}
    {:owner "urbit", :repo "urbit-hob"}
    {:owner "urbit", :repo "urbit-key-generation"}
@@ -284,7 +440,9 @@
    {:owner "tadad" :repo "blog"}
    {:owner "tadad" :repo "blog-ui"}
    {:owner "polrel-witter" :repo "gather"}
+   {:owner "polrel-witter" :repo "live"}
    {:owner "brbenji" :repo "focus"}
+   {:owner "brbenji" :repo "news"}
    {:owner "tiller-tolbus" :repo "cgol"}
    {:owner "divergio" :repo "realtime"}
    {:owner "bacwyls" :repo "radio"}
@@ -292,4 +450,27 @@
    {:owner "bacwyls" :repo "jukebox"}
    {:owner "assemblycapital" :repo "vita"}
    {:owner "assemblycapital" :repo "houston"}
-   {:owner "MarcusMiguel" :repo "classifieds"}])
+   {:owner "MarcusMiguel" :repo "classifieds"}
+   {:owner "uqbar-dao", :repo "dao-contract"}
+   {:owner "uqbar-dao", :repo "escape-app"}
+   {:owner "uqbar-dao", :repo "galaxy-girls"}
+   {:owner "uqbar-dao", :repo "indexer-ui"}
+   {:owner "uqbar-dao", :repo "network-age"}
+   {:owner "uqbar-dao", :repo "phonebook"}
+   {:owner "uqbar-dao", :repo "rdbms"}
+   {:owner "uqbar-dao", :repo "nectar"}
+   {:owner "uqbar-dao", :repo "sirens-of-uqbar"}
+   {:owner "uqbar-dao", :repo "react-native-api"}
+   {:owner "uqbar-dao", :repo "ui-components"}
+   {:owner "uqbar-dao", :repo "uqbar-core"}
+   {:owner "uqbar-dao", :repo "urbit-mobile-app-template"}
+   {:owner "uqbar-dao", :repo "urbit-ui-template"}
+   {:owner "uqbar-dao", :repo "wallet-ui"}
+   {:owner "uqbar-dao", :repo "ziggurat"}
+   {:owner "uqbar-dao", :repo "ziggurat-ui"}
+   {:owner "uqbar-dao", :repo "pongo"}
+   {:owner "uqbar-dao", :repo "pongo-app"}
+   {:owner "uqbar-dao", :repo "fire-hose"}
+   {:owner "uqbar-dao", :repo "dev-suite"}
+   {:owner "uqbar-dao", :repo "screed"}
+   ])
--- src/main.clj
+++ src/main.clj
@@ -1,6 +1,5 @@
 (ns mad.main
-  (:require [mad.data :as data]
-            [next.jdbc :as jdbc]
+  (:require [next.jdbc :as jdbc]
             [honey.sql :as sql]
             [honey.sql.helpers :as sqlh]
             [clj-http.client :as client]
@@ -16,7 +15,7 @@
 (def ds (jdbc/get-datasource db))
 
 (def github-auth-token
-  "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")
+  "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")
 
 (defn create-tables! [ds]
   (do
@@ -35,71 +34,247 @@ CREATE TABLE IF NOT EXISTS commit (
     (jdbc/execute! ds ["
  CREATE TABLE IF NOT EXISTS repository (
     repo_id SERIAL PRIMARY KEY,
+    project TEXT NOT NULL,
+    ecosystem TEXT NOT NULL,
     owner TEXT NOT NULL,
-    repo  TEXT NOT NULL
+    repo TEXT NOT NULL,
+    blacklist BOOLEAN NOT NULL DEFAULT FALSE,
+    UNIQUE (project, ecosystem, owner, repo)
 )"])))
-
 
 (defn drop-tables! [ds]
   (do
     (jdbc/execute! ds ["DROP TABLE IF EXISTS commit"])
     (jdbc/execute! ds ["DROP TABLE IF EXISTS repository"])))
 
-
 (defn reset-db! [ds]
   (do
     (drop-tables! ds)
     (create-tables! ds)))
 
+(defn parse-commits
+  [project ecosystem owner repo response]
+  (update response :body
+          (fn [body]
+            (->> (map (comp
+                       (fn [{:keys [committer message tree author]}]
+                         {:author    (:name author)
+                          :message   message
+                          :repo      repo
+                          :project   project
+                          :ecosystem ecosystem
+                          :owner     owner
+                          :date      (jt/instant->sql-timestamp (jt/instant (:date committer)))
+                          :sha       (:sha tree)})
+                       :commit)
+                      body)))))
 
-(defn fetch-commits [url owner repo project ecosystem]
-  (let [{:keys [body links] :as result}
-        (-> (client/get url {:content-type :json
-                             :query-params {:per_page "100"}
-                             :headers {:Authorization (str "Bearer " github-auth-token)}})
-            (select-keys [:body :links])
-            (update :body #(json/read-str % :key-fn keyword)))]
-    (update result :body
-            (fn [body]
-              (->> (map (comp
-                         (fn [{:keys [committer message tree author]}]
-                           {:author (:name author)
-                            :message message
-                            :repo repo
-                            :project project
-                            :ecosystem ecosystem
-                            :owner owner
-                            :date (jt/instant->sql-timestamp (jt/instant (:date committer)))
-                            :sha (:sha tree)})
-                         :commit)
-                        body))))))
+(defn fetch-commits
+  [url & {:keys [per-page] :or {per-page 100}}]
+  (-> (client/get url
+                  {:content-type :json
+                   :query-params {:per_page (str per-page)}
+                   :headers      {:Authorization (str "Bearer " github-auth-token)}})
+      (update :body #(json/read-str % :key-fn keyword))))
+
+(defn has-commit?
+  "Are `commits` already in the database?"
+  [{sha :sha :as commit}]
+  (seq (jdbc/execute! ds
+                      (-> (sqlh/select :sha)
+                          (sqlh/from :commit)
+                          (sqlh/where [:= :sha sha])
+                          sql/format))))
+
+(defn ecosystem->uri
+  [sub]
+  (let [c (clojure.string/lower-case (first sub))]
+    (format "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/%s/%s.toml"
+            c
+            (-> sub
+                clojure.string/lower-case
+                (clojure.string/replace "." "-")
+                (clojure.string/replace " " "-")))))
+
+(defn extract-owner-repo [s]
+  (let [[owner repo]
+        (->> (clojure.string/split s #"/")
+             (take-last 2))]
+    {:owner owner :repo repo}))
+
+(defn- format-ec-repos
+  [project ecosystem repos]
+  (map (comp #(assoc % :project project :ecosystem ecosystem)
+             extract-owner-repo
+             #(get % "url"))
+       repos))
+
+(defn fetch-ec-repos*
+  [root-url project & [ecosystem]]
+  (try
+    (let [res      (-> (client/get root-url)
+                       :body
+                       toml/read)
+          repos    (format-ec-repos project
+                                    (or ecosystem project)
+                                    (get res "repo"))]
+      (loop [repos    repos
+             sub-ecos (get res "sub_ecosystems")]
+        (if (empty? sub-ecos)
+          repos
+          (recur
+           (let [sub-eco (first sub-ecos)
+                 sub-url (ecosystem->uri sub-eco)]
+             (println
+              (format ">> REPOSITORIES: fetch %s|%s"
+                      project sub-eco))
+             (concat repos (fetch-ec-repos* sub-url project sub-eco)))
+           (rest sub-ecos)))))
+    (catch Exception e
+      (println e)
+      [])))
+
+(defn fetch-ec-repos
+  "Fetch all repositories for the given `project` and recursively fetch all sub-ecosystems.."
+  [project]
+  (println (format "> REPOSITORIES: fetch %s" project))
+  (fetch-ec-repos* (ecosystem->uri project) project))
+
+(defn ingest-new-commits!
+  [{:keys [repository/project
+           repository/ecosystem
+           repository/owner
+           repository/repo
+           repository/blacklist] :as r}
+   & {:keys [delay branch] :or {delay 0}}]
+  (when-not blacklist
+    (println (format "> COMMITS: %s|%s:%s/%s"
+                     project ecosystem owner repo))
+    (loop [url (if branch
+                 (format "http://api.github.com/repos/%s/%s/commits?sha=%s" owner repo branch)
+
+                 (format "http://api.github.com/repos/%s/%s/commits" owner repo))]
+      (let [{:keys [body links]}
+            (parse-commits project ecosystem owner repo (fetch-commits url))]
+        (jdbc/execute! ds (-> (sqlh/insert-into :commit)
+                              (sqlh/values body)
+                              (sqlh/upsert (-> (sqlh/on-conflict :sha)
+                                               (sqlh/do-nothing)))
+                              (sqlh/returning :*)
+                              sql/format))
+        (if-let [next (and (not (has-commit? (last body))) (:next links))]
+          (do
+            (println (format ">> COMMITS: %s/%s at url: %s\n"
+                             owner repo next))
+            (when (> delay 0)
+              (Thread/sleep delay))
+            (recur (:href next)))
+          :done)))))
 
 
-(defn fetch-and-write-commits! [ds owner repo project ecosystem]
-  (loop [url (format "http://api.github.com/repos/%s/%s/commits" owner repo)]
-    (let [{:keys [body links]}
-          (fetch-commits url owner repo project ecosystem)]
-      (jdbc/execute! ds (-> (sqlh/insert-into :commit)
-                            (sqlh/values body)
-                            (sqlh/upsert (-> (sqlh/on-conflict :sha)
-                                             (sqlh/do-nothing)))
-                            (sqlh/returning :*)
-                            sql/format))
-      (if-let [next (:next links)]
-        (do
-          (println url)
-          (recur (:href next)))
-        :done))))
+(defn ingest-repositories!
+  [repositories]
+  (let [result (jdbc/execute! ds (-> (sqlh/insert-into :repository)
+                                     (sqlh/values repositories)
+                                     (sqlh/upsert (-> (sqlh/on-conflict :owner :repo :project :ecosystem)
+                                                      (sqlh/do-nothing)))
+                                     (sqlh/returning :*)
+                                     sql/format))]
+    (println (format "REPOSITORIES: wrote %s" (count result)))
+    :done))
+
+(comment
+
+  (ingest-repositories!
+   (mapv
+    (fn [{:keys [owner repo]}]
+      {:repository/owner owner
+       :repository/repo repo
+       :repository/project "urbit"
+       :repository/ecosystem "urbit"
+       :repository/blacklist false})
+    mad.data/known-repositories))
+
+  (refresh-project! "urbit" {})
+
+  (ingest-new-commits!
+   {:repository/project "urbit"
+    :repository/ecosystem "urbit"
+    :repository/owner "urbit"
+    :repository/repo "urbit"})
+
+  (ingest-new-commits!
+   {:repository/project "urbit"
+    :repository/ecosystem "urbit"
+    :repository/owner "urbit"
+    :repository/repo "vere"})
+
+  )
+
+(defn refresh-project!
+  [project {:keys [refresh-repos? delay]
+            :or   {refresh-repos? false
+                   delay          0}}]
+  (when refresh-repos?
+    (ingest-repositories! (fetch-ec-repos project)))
+  (let [repositories
+        (jdbc/execute! ds (-> (sqlh/select :*)
+                              (sqlh/from :repository)
+                              (sqlh/where [:= :project project])
+                              sql/format))]
+    (doseq [r repositories]
+      (try
+        (ingest-new-commits! r :delay delay)
+        (catch Exception e
+          (println e))))))
+
+(defn refresh-projects!
+  [& {:as opts}]
+  (doseq [{project :repository/project}
+          (jdbc/execute! ds (-> (sqlh/select-distinct :project)
+                                (sqlh/from :repository)
+                                (sql/format)))]
+    (refresh-project! project opts)))
+
+;; OPTIMIZATIONS TO MAKE
+;; x stop fetching commits when last commit is already in DB
+;;   x verify that commits are not in order of descending time
+;; x store repositories in table
+;;   x add column to blacklist a repository
+;; x separate repo fetching and commit fetching into separate steps
+;; * create blacklists
+
+(def all-projects
+  ["cardano"
+   "polygon"
+   "chainlink"
+   "filecoin"
+   "starknet"
+   "internet-computer"
+   "aptos"
+   "near"
+   "optimism"
+   "avalanche"
+   "solana"])
+
+(defn load-all-repositories!
+  [& [projects]]
+  (doseq [project (or projects all-projects)]
+    (doseq [rs (partition 1000 1000 nil (fetch-ec-repos project))]
+      (ingest-repositories! rs))))
 
 (comment
 
   ;; initialize database
-  (reset-db! ds)
+  ;; (reset-db! ds)
 
-  ;; load all urbit repositories into db
-  (doseq [{:keys [owner repo]} (reverse data/known-repositories)]
-    (try (fetch-and-write-commits! ds owner repo "urbit" "urbit")
-         (catch Exception e
-           (println e))))
+  ;; load all repositories into db
+  (load-all-repositories!)
 
-  )
+  (refresh-projects! :delay 1500)
+
+  (refresh-project! "urbit" {})
+
+  (do
+    (refresh-project! "internet-computer" {:delay 2000})
+    (refresh-project! "starknet" {:delay 2000})))
